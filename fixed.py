75f7f7bb3bcebea27955e9c136370c0c1419d81f
Muhammad Aamir Zaman <amir.zaman@codefulcrum.com>
2021-12-24 14:24:25 +0500

    bug fixed and else added

---
+++ fixed.py
@@ -0,0 +1,14 @@
+
+age = int(input('Enter Age:'))
+
+if age > 9 and age < 21:
+    print('You are Kid')
+
+elif age > 20 and age < 35:
+    print('You are Young')
+
+elif age > 34 and age < 121:
+    print('You are Old')
+
+else:
+    print('You are not Human')
--- main.py
+++
@@ -1,7 +0,0 @@
-age = int(input('Enter Age:'))
-
-if age >= 10 and age <= 20:
-    print('You are Kid')
-if age >= 21:
-    print('You are Old')
-
