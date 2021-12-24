6a53305d257ec8f3ce7ea6b2450a4703c2f6dae3
Muhammad Aamir Zaman <amir.zaman@codefulcrum.com>
2021-12-24 17:48:06 +0500

    new msg for specific age range

--- fixed.py
+++ fixed.py
@@ -1,12 +1,16 @@
 age = int(input('Enter Age:'))
 
+
 if age > 9 and age < 21:
     print('You are Kid')
 
 elif age > 20 and age < 35:
     print('You are Young')
 
-elif age > 34 and age < 121:
+elif age > 34 and age < 51:
+    print('You are Man')
+
+elif age > 50 and age < 121:
     print('You are Old')
 
 else:
