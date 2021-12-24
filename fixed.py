c407f1b8ad03d6e290b76f35af7dcee2d4a0142e
Muhammad Aamir Zaman <amir.zaman@codefulcrum.com>
2021-12-24 18:37:19 +0500

    add new if for 5-9 years kids

--- fixed.py
+++ fixed.py
@@ -3,6 +3,9 @@ age = int(input('Enter Age:'))
 if age > 1 and age < 4:
     print('You are baby')
 
+elif age > 4 and age < 10:
+    print('You are cute')
+
 elif age > 10  and age < 20:
     print('You are Kid')
 
