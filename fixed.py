0e571c334e1096b6ccb0b8b4b944ff0b1c222120
Muhammad Aamir Zaman <amir.zaman@codefulcrum.com>
2021-12-24 18:15:37 +0500

    add a new if from bug_fix using cherry-pick

--- fixed.py
+++ fixed.py
@@ -1,6 +1,9 @@
 age = int(input('Enter Age:'))
 
-if age > 9 and age < 21:
+if age > 1 and age < 4:
+    print('You are baby')
+
+elif age > 10  and age < 20:
     print('You are Kid')
 
 elif age > 20 and age < 35:
